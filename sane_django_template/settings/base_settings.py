76f98c402d3717c518c6bfef860fd6cc65908278
Bradley Marques <bradleyrcmarques@gmail.com>
2023-09-10 20:48:51 +0200

    .

--- sane_django_template/settings/base_settings.py
+++ sane_django_template/settings/base_settings.py
@@ -168,7 +168,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@example.com")
 
 APPEND_SLASH = True  # Append trailing slash to URLs that don't have them
 
